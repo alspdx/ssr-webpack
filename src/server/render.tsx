d436787daa00be071c0e5a14cbc29034ca5275b5
Adam Smith <25121879+alspdx@users.noreply.github.com>
2024-04-22 10:33:24 -0700

    extract static HTML generation to function

--- src/server/render.tsx
+++ src/server/render.tsx
@@ -5,13 +5,12 @@ import { Request, Response } from 'express';
 
 import { routes } from 'routes';
 import { LoaderContextProvider, loadRouteData, getRouteLoaders } from 'context/LoaderContext';
+import { renderStaticHtml } from './renderStaticHtml';
 
-export default function render(req: Request, res: Response, assetPaths: { styles: string[]; scripts: string[] }) {
+export default function render(req: Request, res: Response, assets: { styles: string[]; scripts: string[] }) {
   match(
     { routes, location: req.originalUrl },
     async (error, redirectLocation, renderProps) => {
-      console.log('renderProps: ', renderProps);
-
       if (redirectLocation) {
         console.log('Redirecting to:', redirectLocation.pathname + redirectLocation.search);
         res.redirect(redirectLocation.pathname + redirectLocation.search);
@@ -22,37 +21,16 @@ export default function render(req: Request, res: Response, assetPaths: { styles
         const loaders = getRouteLoaders(renderProps.components);
         const preloadedData = await loadRouteData(loaders);
 
-        res.status(200).send(`<!doctype html>
-<html>
-  <head>
-    <title>React SSR Example</title>
-    ${assetPaths.styles.map((path) => `<link rel="stylesheet" href="${path}" />`).join('\n')}
-    <link
-      rel="shortcut icon"
-      type="image/png"
-      href="${__PUBLIC_PATH_PREFIX__}favicon.ico?v=2"
-    ></link>
-    <link
-      rel="shortcut icon"
-      type="image/png"
-      sizes="32x32"
-      href="${__PUBLIC_PATH_PREFIX__}favicon-32x32.png?v=2"
-    />
-    <link
-      rel="shortcut icon"
-      type="image/png"
-      sizes="16x16"
-      href="${__PUBLIC_PATH_PREFIX__}favicon-16x16.png?v=2"
-    />
-  </head>
-  <body>
-    <script>
-      window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedData)};
-    </script>
-    <div id="root">${ReactDOM.renderToString(<LoaderContextProvider routerProps={renderProps} preloadedState={preloadedData} />)}</div>
-    ${assetPaths.scripts.map((path) => `<script src="${path}"></script>`).join('\n')}
-  </body>
-</html>`);
+        const content = ReactDOM.renderToString(
+          <LoaderContextProvider
+            routerProps={renderProps}
+            preloadedState={preloadedData}
+          />
+        )
+
+        const html = renderStaticHtml({ assets, content, preloadedData });
+
+        res.status(200).send(html);
       } else {
         res.status(404).send('Not found');
       }
---
+++ src/server/renderStaticHtml.ts
@@ -0,0 +1,56 @@
+interface RenderParams {
+  /**
+   * optional pre-rendered content to be injected into the HTML
+   */
+  content?: string;
+  /**
+   * asset paths used to generate <link> and <script> tags
+   */
+  assets: Record<'styles' | 'scripts', string[]>;
+  /**
+   * preloaded data for each of the component loaders
+   */
+  preloadedData: Record<string, unknown>;
+}
+
+export function renderStaticHtml({ assets, preloadedData, content }: RenderParams) {
+  const STYLES = assets.styles
+    .map((path) => `<link rel="stylesheet" href="${path}">`)
+    .join('\n');
+
+  const SCRIPTS = assets.scripts
+    .map((path) => `<script src="${path}"></script>`)
+    .join('\n');
+
+  return `<!doctype html>
+<html>
+  <head>
+    <title>React SSR Example</title>
+    ${STYLES}
+    <link
+      rel="shortcut icon"
+      type="image/png"
+      href="${__PUBLIC_PATH_PREFIX__}favicon.ico?v=2"
+    ></link>
+    <link
+      rel="shortcut icon"
+      type="image/png"
+      sizes="32x32"
+      href="${__PUBLIC_PATH_PREFIX__}favicon-32x32.png?v=2"
+    />
+    <link
+      rel="shortcut icon"
+      type="image/png"
+      sizes="16x16"
+      href="${__PUBLIC_PATH_PREFIX__}favicon-16x16.png?v=2"
+    />
+  </head>
+  <body>
+    <div id="root">${content}</div>
+    <script>
+      window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedData)};
+    </script>
+    ${SCRIPTS}
+  </body>
+</html>`;
+}
